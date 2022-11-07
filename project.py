fee869cb468ba328814f96a1f404cfed7e3dd961
Vkmstry1611 <89677101+Vkmstry1611@users.noreply.github.com>
2022-11-07 21:58:03 +0530

    update

--- project.py
+++ project.py
@@ -1,3 +1,59 @@
+#importing packages
+
 import pandas as pd
 
-pd.read_csv("flight.csv")
+def cust_mangement():
+    while True:
+        print("1) \t Display all Customer Records")
+        print("2) \t Search Customer Record")
+        print("3) \t Update Customer Record")
+        print("4) \t Delete Customer Record")
+        print("5) \t exit")
+        
+        op=int(input("Enter your Choice :  "))
+
+        if op==1:
+            print()
+        elif op==2:
+            print()
+        elif op==3:
+            print()
+        elif op==4:
+            print()
+        elif op==5:
+            break
+        else:
+            print("Invalid Option")
+
+
+def book():
+    while True:
+        print()
+
+    
+
+        
+
+#print(pd.read_csv("D:/Study/12th/Project/IP/Practical/IP-Project/flight.csv"))
+
+while True:
+    print("1) \t View Booking Details")
+    print("2) \t View Flight Schedules")
+    print("3) \t Book Seats")
+    print("4) \t Customer Management")
+    print("5) \t Exit")
+
+    opt=int(input("Enter Your Choice :  "))
+    
+    if opt==1:
+        print("")
+    elif opt==2:
+        print("")    
+    elif opt==3:
+        print("")
+    elif opt==4:
+        print("")
+    elif opt==5:
+        break
+    else:
+        print("Invalid Option") 
