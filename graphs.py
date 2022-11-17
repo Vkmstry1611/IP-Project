a249af760327dfeb5ea3e6f887730dea512f707d
Kushagra Agarwal <67466389+KushagraAgarwal525@users.noreply.github.com>
2022-11-18 00:39:24 +0530

    Add graph 1

--- graphs.py
+++ graphs.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
+import mysql.connector as sql
 
+db=sql.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+	database = "db"
+)
+
+cursor=db.cursor()
+
+def customer_vs_tickets graph:
+	cursor.execute("SELECT c.cust_id, COUNT(*) FROM customer_details AS c JOIN booking_details ON booking_details.cust_id = c.cust_id GROUP BY c.cust_id;")
+	result = cursor.fetchall()
+	plt.plot(result[0], result[1])
+	plt.xlabel("User") #add the Label on x-axis
+	plt.ylabel("No. of Bookings") #add the Label on y-axis
+	plt.title("User vs Bookings graph")
+	plt.show()
